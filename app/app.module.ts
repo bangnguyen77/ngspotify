6cb979ebdc7f8f8c205d963000dff8dbe2bc759a
Bang Nguyen <bangnv77@gmail.com>
2016-11-29 11:33:20 -0500

    create navbar

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,9 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
+import { SearchComponent } from './components/search/search.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ 
+    AppComponent,
+    SearchComponent,
+    AboutComponent,
+    NavbarComponent 
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
